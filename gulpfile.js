39aa0c1749662baf8d2e118ce89e1a2666957a61
SergioSanchezPerez19 <s.sanchezperezgarcia@gmail.com>
2024-03-27 17:03:29 +0100

    SeSa: Add html and css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     // compilar sass
@@ -20,13 +23,31 @@ function dev() {
 }
 
 function imagenes() {
-    return src('src/img/**/*').pipe(dest('build/img'));
+    return src('src/img/**/*')
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'));
+}
+
+function versionWebp() {
+    const opciones = {
+        quality: 50,
+    };
+    return src('src/img/**/*.{png,jpg}').pipe(webp(opciones)).pipe(dest('build/img'));
+}
+
+function versionAvif() {
+    const opciones = {
+        quality: 50,
+    };
+    return src('src/img/**/*.{png,jpg}').pipe(avif(opciones)).pipe(dest('build/img'));
 }
 
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
-exports.default = series(imagenes, css, dev);
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
 
 // Series se inicia una tarea, y hasta que finaliza, inicia la siguiente
 // Parallel inicia todas a la vez
